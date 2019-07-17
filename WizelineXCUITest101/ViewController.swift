7ace2d39f0773c44142c52a119b29f1437184f61
JorgeOrduno <jorge.orduno@wizeline.com>
2019-07-17 09:56:04 -0500

    Initial Commit

---
+++ WizelineXCUITest101/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WizelineXCUITest101
+//
+//  Created by Jorge Orduno on 7/17/19.
+//  Copyright © 2019 Jorge Orduno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
